3cb35389538d1120950c1578c3994e5244f4ca8c
plugmcpe <M7mmdgamer7@icloud.com>
2016-08-18 23:53:55 +0300

    Create main.php

---
+++ src/main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace heal
+
+use pocketmine/plugin/PluginBase
+use pocketmine/utils/TextFormat
+use pocketmine/command/CommandSender
+use pocketmine/command/Command
+
+ class Main extends PluginBase{
+public function onEnable() {
+   $this->getserver()->getLogger()->info(Color::GREEN,"(healpluginOn)"
+
+
+    public function onDisable() {
+   $this->getserver()->getLogger()->info(Color::RED,"(healpluginOff)"
+
+
+public function oncommand(CommandSender $sender, command $cmd, $lable, array $args){
+   switch($cmd->getName())( /* Switch == Case || $cmd->getName() == command */
+    case heal:
+    $sender->sendMassage(Color::BLACK,"your are heal your health"
+     $sender->getheal()->20();
